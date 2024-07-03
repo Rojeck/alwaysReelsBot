aac2561b5ec4b2309d1f7cb5c5750bc4f7cbd3d0
Bohdan Beshlei <30295583+Rojeck@users.noreply.github.com>
2024-07-03 03:12:55 +0300

    feat: add pm2 config (#11)
    Co-authored-by: BohdanMW <bogdan.b@maybeworks.dev>

---
+++ pm2.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'arb', 
+        script: './dist/main.js', 
+        instances: 1, 
+        autorestart: true, 
+        watch: false, 
+        max_memory_restart: '1G', 
+      },
+    ],
+  };
